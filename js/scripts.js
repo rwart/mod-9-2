22ea9538f124210aacae6740f3b2e5e1c03235c2
rwart <32432397+rwart@users.noreply.github.com>
2017-10-12 20:21:28 +0200

    for review

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
+function addUniques(arr, item) {
+  'use strict';
+  if (arr.indexOf(item) === -1) {
+    arr.push(item);
+  }
 
+  return arr;
+}
+
+function Main() {
+  'use strict';
+  var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+  var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+  var allNames = femaleNames.concat(maleNames);
+  console.log('allNames: ' + allNames.join(', '));
+  var newName = 'Marian';
+
+  if (allNames.indexOf(newName) === -1) {
+    allNames.push(newName);
+  }
+
+  console.log('allNames after adding ' + newName + ' : ' + allNames.join(', '));
+
+  /* the same with function */
+
+  var allNamesNew = addUniques(allNames, newName);
+  console.log('function: Add again ' + newName + ' : ' + allNamesNew.join(', '));
+  newName = 'Ania';
+  allNamesNew = addUniques(allNames, newName);
+  console.log('function: Add ' + newName + ' : ' + allNamesNew.join(', '));
+}
+
+Main();
